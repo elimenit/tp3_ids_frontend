bda47ae3d14626c08253e8f268ff95babbf3b61a
longotiziano <longo.tiziano.arg@gmail.com>
2026-05-21 11:40:42 -0300

    Creacion inicial del nav y registro de rutas login y signup

--- app.py
+++ app.py
@@ -2,6 +2,10 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# Blueprints
+from routers.login import public_login_bp
+app.register_blueprint(public_login_bp)
+
 @app.route("/", methods=['GET'])
 def main():
     return render_template('public/index.html')
